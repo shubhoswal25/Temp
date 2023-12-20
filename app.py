ddec00b4de31e4396473bfafde12ab06a79ceb38
Shubh Oswal <soswal2@wisc.edu>
2023-12-20 00:43:12 -0600

    pushed

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def read_text_from_file(file_path):
     with open(file_path, 'r') as file:
         return file.read().strip()
-
+    
 # Function to get the next option from a file
 def get_next_option(file_path):
     with open(file_path, 'r') as file:
@@ -21,10 +21,10 @@ def get_next_option(file_path):
     return story, options
 
 # Specify the paths to your text files
-background_image = "static/background.jpg"  # Update with your image file path
+background_image = "static/background.png"  # Update with your image file path
 
 current_option = {
-    'file': "static/start.txt",
+    'file': "static/extra/start.txt",
 }
 
 @app.route('/')
@@ -36,34 +36,19 @@ def index():
 def button_clicked():
     selected_button = request.form['button']
     print(selected_button)
-    # if selected_button == 'button_1' or selected_button == 'button_2':
-    #     current_option['file'] = current_option[selected_button+'_file']
-    #     current_option['file'] = current_option['file']
-    #     print(f'current option {current_option}')
 
-
-    # else:
-    #     return render_template('index.html', text="Unknown option", background_image=background_image)
     story, options = get_next_option(current_option['file'])
-    # print(f"story: {story}")
-    # print(f"len(options): {len(options)}")
-    # print(f"options: {options}")
-
-    # if selected_button == 'button_1' or selected_button == 'button_2':
+    
     if selected_button == 'button_1':
-        current_option['file'] = 'static/'+options[0]['file']
+        current_option['file'] = 'static/extra/'+options[0]['file']
         file_text = read_text_from_file(current_option['file'])
         # print(f"current_option['file']: {current_option['file']}")
         story, options = get_next_option(current_option['file'])
     else:
-        current_option['file'] = 'static/'+options[1]['file']
+        current_option['file'] = 'static/extra/'+options[1]['file']
         # print(f"current_option['file']: {current_option['file']}")
         file_text = read_text_from_file(current_option['file'])
         story, options = get_next_option(current_option['file'])
-
-    # else:
-    #     print('ran')
-    #     return render_template('index.html', text="Unknown option", background_image=background_image)
 
     if options !=[]:
         # In this alternative, there are options
@@ -74,5 +59,17 @@ def button_clicked():
         # print(f'2. story {story}')
         return render_template('index.html', story=story, background_image=background_image, current_option = current_option)
 
+@app.route('/quit_story')
+def quit_story():
+    return render_template('goodbye.html')  # Create a 'goodbye.html' template for the goodbye page
+
+# Reset the story route
+@app.route('/reset_story')
+def reset_story():
+    current_option['file'] = "static/extra/start.txt"  # Reset to the initial story file
+    story, options = get_next_option(current_option['file'])
+    return render_template('index.html', story=story, options=options, background_image=background_image, current_option=current_option)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
